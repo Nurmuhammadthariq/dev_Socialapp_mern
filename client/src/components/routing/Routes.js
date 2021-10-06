05e866d404907a89e76cb0ae1639d9779809371c
thoriqnoer <thoriq.noer@gmail.com>
2021-10-06 07:59:32 +0700

    Add alertcard in row to display

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
+import AlertCard from '../layout/AlertCard';
+import { Col, Row } from 'react-bootstrap';
 
 const Routes = () => {
   return (
     <section className="container">
+      <Row className="justify-content-md-center">
+        <Col lg={5}>
+          <AlertCard />
+        </Col>
+      </Row>
       <Switch>
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
