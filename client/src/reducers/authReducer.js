74cd8e391137cd2adb871ecf943af1374ef9e2b0
thoriqnoer <thoriq.noer@gmail.com>
2021-10-09 15:47:36 +0700

    Create auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,11 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGOUT,
+} from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -7,6 +14,41 @@ const initialState = {
   user: null,
 };
 
-const authReducer = () => {};
+const authReducer = (state = initialState, action) => {
+  const { payload, type } = action;
+
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload,
+      };
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+      localStorage.setItem('token', payload.token);
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+    case REGISTER_FAIL:
+    case AUTH_ERROR:
+    case LOGOUT:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export default authReducer;
