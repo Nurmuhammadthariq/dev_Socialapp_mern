dba80dd041551b75222c324043acd19d7e2d194b
thoriqnoer <thoriq.noer@gmail.com>
2021-10-09 15:45:21 +0700

    set action load user

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Landing from './components/layout/Landing';
 import Routes from './components/routing/Routes';
 import './App.css';
 // Redux
-import { Provider } from 'react-redux'
-import store from './store'
-
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/authAction';
+import setAuthToken from './utils/setAuthToken';
 
 const App = () => {
+  useEffect(() => {
+    // check for token in local storage
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
@@ -20,7 +29,7 @@ const App = () => {
             <Route component={Routes} />
           </Switch>
         </Fragment>
-    </Router>
+      </Router>
     </Provider>
   );
 };
