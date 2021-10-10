570ff4773330a705808806ad03b1e7db4ac3a8a8
thoriqnoer <thoriq.noer@gmail.com>
2021-10-10 21:10:45 +0700

    Create page dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Button } from 'react-bootstrap';
 
-const Dashboard = (props) => {
-  return <div>Dashboard</div>;
+//Redux
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profileAction';
+
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return (
+    <div>
+      <h3>Dashboard</h3>
+      <p className="lead">
+        <i className="fa fa-user"></i> Welcome!{' '}
+        <span className="text-color-g"> {user && user.name} </span>
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <div className="my-5">
+            <Button variant="danger">Delete your account</Button>
+          </div>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not yet setup a profile, please add some info</p>
+          <Link to="#" className="btn btn-primary my-1">
+            Create your profile
+          </Link>
+        </Fragment>
+      )}
+    </div>
+  );
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateProps = (state) => ({
+  auth: state.authReducer,
+  profile: state.profileReducer,
+});
+
+export default connect(mapStateProps, { getCurrentProfile })(Dashboard);
