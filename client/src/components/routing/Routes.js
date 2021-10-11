6363d4b8228c0c5edce75b72fc45a972a906d8d0
thoriqnoer <thoriq.noer@gmail.com>
2021-10-11 07:21:55 +0700

    Create profile form component

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../dashboard/Dashboard';
+import ProfileForm from '../profileForms/ProfileForm';
 import Register from '../auth/Register';
 import Login from '../auth/Login';
 import AlertCard from '../layout/AlertCard';
+import PrivateRoute from './PrivateRoute';
 
 import { Col, Row } from 'react-bootstrap';
-import PrivateRoute from './PrivateRoute';
 
 const Routes = () => {
   return (
@@ -20,6 +21,7 @@ const Routes = () => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute exact path="/createProfile" component={ProfileForm} />
       </Switch>
     </section>
   );
