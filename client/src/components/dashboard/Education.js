f602d3627c4153aee58707c6986ef7168d016a52
thoriqnoer <thoriq.noer@gmail.com>
2021-10-19 06:59:45 +0700

    add action delete education

--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { deleteEducation } from '../../actions/profileAction';
 import formatDate from '../../utils/formatDate';
-import { Table, Row, Col } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
 
-const Education = ({ education }) => {
+const Education = ({ education, deleteEducation }) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
@@ -13,35 +14,37 @@ const Education = ({ education }) => {
         {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : 'Present'}
       </td>
       <td>
-        <button className="btn btn-danger">Remove</button>
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className="btn btn-danger"
+        >
+          Remove
+        </button>
       </td>
     </tr>
   ));
 
   return (
     <Fragment>
-      <Row>
-        <Col lg={12} md={6} sm={3}>
-          <h4>Education Credentials</h4>
-          <Table className="table" responsive>
-            <thead>
-              <tr>
-                <th>Institute</th>
-                <th>Degree</th>
-                <th>Years</th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>{educations}</tbody>
-          </Table>
-        </Col>
-      </Row>
+      <h4>Education Credentials</h4>
+      <Table className="table" responsive>
+        <thead>
+          <tr>
+            <th>Institute</th>
+            <th>Degree</th>
+            <th>Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </Table>
     </Fragment>
   );
 };
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default connect(null)(Education);
+export default connect(null, { deleteEducation })(Education);
