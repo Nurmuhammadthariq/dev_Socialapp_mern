e493507a7fd2cda48e1ebc64a5944b8e414390c1
thoriqnoer <thoriq.noer@gmail.com>
2021-10-21 07:35:39 +0700

    set color theme

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const colors = require('colors');
 
 const app = express();
+
+colors.setTheme({
+  custom: ['black', 'underline', 'bgWhite'],
+});
 
 //Connect Database
 connectDB();
@@ -19,4 +24,4 @@ app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.custom));
