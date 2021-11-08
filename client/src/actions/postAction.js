fb7509f73eb225e49be968d6c35b83c1b3c5d44e
thoriqnoer <thoriq.noer@gmail.com>
2021-11-08 07:05:03 +0700

    Get Singlel Post

--- client/src/actions/postAction.js
+++ client/src/actions/postAction.js
@@ -7,6 +7,7 @@ import {
   UPDATE_LIKES,
   DELETE_POST,
   ADD_POST,
+  GET_POST,
 } from './types';
 
 // Get posts
@@ -100,6 +101,27 @@ export const addPost = (formData) => async (dispatch) => {
     });
 
     dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText },
+    });
+  }
+};
+
+// Get post
+export const getPost = (id) => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+
+    dispatch({
+      type: GET_POST,
+      payload: res.data,
+    });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
