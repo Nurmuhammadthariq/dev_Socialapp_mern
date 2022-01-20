10c32f37e897f90c098b256386cecbe49143b999
thoriqnoer <thoriq.noer@gmail.com>
2022-01-20 19:44:53 +0700

    Changer brand and routing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,16 +6,16 @@ const Landing = () => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="head">Developer Community</h1>
+          <h1 className="head">Social Network for Developer</h1>
           <p className="lead">
             Use this platform with impressive portfolio and engage with other
             developers
           </p>
           <div className="buttons">
-            <Link to="/register.html" className="btn btn-signup">
+            <Link to="/register" className="btn btn-signup">
               SIGN UP
             </Link>
-            <Link to="/login.html" className="btn btn-login">
+            <Link to="/login" className="btn btn-login">
               LOGIN
             </Link>
           </div>
